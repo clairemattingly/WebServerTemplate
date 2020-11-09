5e081f5711d5015e6dd7959d42467e84276e2bd8
JHopper21 <JHopper21@holynames-sea.org>
2020-11-09 07:46:28 -0800

    Created environment variables

---
+++ SQL/SQLQuery_1.sql
@@ -0,0 +1,5 @@
+DROP TABLE Student;
+CREATE TABLE Student(Name TEXT DEFAULT 'Anonymous', ClassYear INTEGER NOT NULL, Recommendation TEXT, Why Text);
+SELECT * FROM Student
+
+
---
+++ SQL/SQLQuery_2.sql
@@ -0,0 +1,7 @@
+INSERT INTO Student (Name, ClassYear, Recommendation, Why)
+VALUES ('Julia', '2021', 'Yes', 'good teacher');
+INSERT INTO Student (Name, ClassYear, Recommendation, Why)
+VALUES ('Bri', '2009', 'No', 'talked too fast');
+INSERT INTO Student (ClassYear, Recommendation, Why)
+VALUES ('2022', 'Yes', 'Not a lot of tests');
+SELECT * FROM Student
