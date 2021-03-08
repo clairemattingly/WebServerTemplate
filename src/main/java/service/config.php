e8e2931484556a3e666c6f518f8d9d717c00417c
JHopper21 <JHopper21@holynames-sea.org>
2021-03-07 18:52:35 -0800

    Connected database to HTML and found way to edit and connect it

---
+++ src/main/java/service/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbhost='holynamesacademy.database.windows.net';
+$dbname='GlassDome';
+$dbusername='hna-admin';
+$dbpass='HolyNames123';
+
+$mysqli=mysqli_connect($dbhost, $dbusername, $dbpass, $dbname);
+?>
